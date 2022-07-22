61856598da412663002e7c77603b7b7b1b92511a
kyiroywops <aleexisazul@gmail.com>
2022-07-22 01:14:18 -0400

    index cambio

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,39 @@
 import React from "react";
 
 import PageWrapper from "../components/PageWrapper";
-import Hero from "../sections/home1/Hero";
-import Company from "../sections/home1/Compnay";
-import Categories from "../sections/home1/Categories";
-import Jobs from "../sections/home1/Jobs";
-import Content1 from "../sections/home1/Content1";
-import Blog from "../sections/home1/Blog";
-// import Section from "../layout/Section";
+import Hero from "../sections/home4/Hero";
+import ContentOne from "../sections/home4/ContentOne";
+import ContentTwo from "../sections/home4/ContentTwo";
+import ContentThree from "../sections/home4/ContentThree";
+import Team from "../sections/home4/Team";
+import Testimonial from "../sections/home4/Testimonial";
+import Pricing from "../sections/home4/Pricing";
 
-const IndexPage = () => {
+const Home3 = () => {
   return (
     <>
-      <PageWrapper>
-        <Hero className="position-relative z-index-1" />
-        <Company className="bg-default-1 pt-13 pt-md-17 pt-lg-24 pb-13 pb-md-14 pb-lg-23" />
-        <Categories className="bg-default-1 pt-lg-13 pb-md-7 pb-lg-11 position-relative" />
-        <Jobs className="pt-14 pt-md-18 pt-lg-30 bg-default-2 pb-9 pb-md-13 pb-lg-29" />
-        <Content1 className="pt-21 pt-md-24 pt-lg-32 pb-15 pb-md-19 pb-lg-30" />
-        <Blog className="bg-default-1 pt-14 pt-md-18 pt-lg-27 pb-13 pb-md-17 pb-lg-26" />
+      <PageWrapper
+        themeConfig={{
+          headerClassName: "site-header--menu-right dark-mode-texts dropdown-left",
+          headerButton: (
+            <>
+              <button className="btn btn btn-white btn-medium rounded-5 font-size-3">
+                Start 14 Days trial
+              </button>
+            </>
+          ),
+          footerStyle: "style4",
+        }}
+      >
+        <Hero className="bg-blue-1 position-relative pt-20 pt-sm-24 pt-lg-34 pb-9 pb-lg-32" />
+        <ContentOne className="pt-13 pt-lg-28 pb-13 pb-lg-26" />
+        <ContentTwo className="bg-default-4 pt-15 pt-lg-30 pb-15 pb-lg-26" />
+        <ContentThree className="bg-dark-cloud pt-30 pt-lg-28 pb-15 pb-lg-30" />
+        <Team className="pt-13 pt-lg-24 pb-15 pb-lg-25" />
+        <Testimonial />
+        <Pricing className="bg-sunset pt-14 pt-lg-24 pb-5 pb-lg-27" />
       </PageWrapper>
     </>
   );
 };
-export default IndexPage;
+export default Home3;
